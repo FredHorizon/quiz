016f4edbd5ddb2ddcc5200e1b323761db7b3dcb8
FredHorizon <fredoliveira.sys@gmail.com>
2021-01-26 12:41:03 -0400

    Test example

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
 import styled from 'styled-components'
 
 const Title = styled.h1`
+  display: flex;
+  justify-content: center;
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
 
+const TitleTwo = styled.h2`
+  display: flex;
+  justify-content: center;
+  font-size: 30px;
+  color: ${({ theme }) => theme.colors.primary};
+`
+
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <>
+      <Title>My Title</Title>
+      <TitleTwo>My subtitle</TitleTwo>
+    </>
+  )
 }
