6daae081778d3e2c67baee7a7be333ac3e042d68
FredHorizon <fredoliveira.sys@gmail.com>
2021-01-26 14:46:22 -0400

    Config background image index

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,17 @@
 import styled from 'styled-components'
+import db from '../db.json';
 
-const Title = styled.h1`
-  display: flex;
-  justify-content: center;
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
-
-// O Styled component cria essa função com props e children automaticamente
-// function Title(props) {
-//   return <h1>{props.children}</h1>
-// }
+const BackgroundImage = styled.div`
+ background-image: url(${db.bg});
+ flex: 1;
+ background-size: cover;
+ background-position: center;
+`;
 
 export default function Home() {
   return (
-    <>
-      <Title>My page</Title>
-    </>
-  )
+    <BackgroundImage>
+      adasdasd
+    </BackgroundImage>
+  );
 }
